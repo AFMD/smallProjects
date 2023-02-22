b693cd6bd3a248cc548ca908956b25ddbce0a889
Greyson Christoforo <grey@christoforo.net>
2023-02-22 11:43:57 +0000

    add some dfs mount notes about /etc/hosts

--- mountDFS/mountDFS.sh
+++ mountDFS/mountDFS.sh
@@ -11,8 +11,17 @@ THIS_USER=`pstree -lu -s $$ | grep --max-count=1 -o '([^)]*)' | head -n 1 | sed 
 MOUNT_POINT=/mnt/dfs
 mkdir -p "${MOUNT_POINT}"
 
+# /etc/hosts must be proper for this to work, the mount looks up "DC6" and that must match IPs
+# so we can do
+# echo "163.1.74.116 DC6" >> /etc/hosts
+# where that ip is for dc6.physics.ox.ac.uk
+
 CIFS_SERVER="dc6.physics.ox.ac.uk"
+#CIFS_SERVER="STARR.physics.ox.ac.uk"
 #CIFS_SERVER="physics.ox.ac.uk"
+#CIFS_SERVER="163.1.245.185"
+#CIFS_SERVER="163.1.74.123"
+#CIFS_SERVER="163.1.74.116"
 
 while : ; do
   umount "${MOUNT_POINT}" > /dev/null 2>&1 || true
